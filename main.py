b768b8146c8f5587384c8aac697562dc3e1f52e5
Mustafa Bebek <mmustafab324@gmail.com>
2023-09-18 16:43:39 +0300

    generate bucket url request

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import concurrent.futures
+import requests
+import time
+from cloud_providers.aws_regions import REGIONS
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+CONNECTIONS = 100000
+TIMEOUT = 5
 
+def get_user_input():
+    user_input = input("Please enter a bucket name: ")
+    return user_input.strip()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def fetch_web_pages(bucket_name, timeout):
+    urls = [f'https://{bucket_name}.s3.{region}.amazonaws.com' or f'https://{bucket_name}.s3-{region}.amazonaws.com' for region in REGIONS]
 
+    def check_url(url):
+        try:
+            response = requests.get(url, timeout=timeout)
+            return url, response.status_code == 200
+        except requests.RequestException:
+            return url, False
 
-# Press the green button in the gutter to run the script.
+    with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
+        future_to_url = {executor.submit(check_url, url): url for url in urls}
+        time1 = time.time()
+        accessible_urls = []
+        inaccessible_urls = []
+
+        for future in concurrent.futures.as_completed(future_to_url):
+            url = future_to_url[future]
+            url, is_accessible = future.result()
+            if is_accessible:
+                accessible_urls.append(url)
+            else:
+                inaccessible_urls.append(url)
+
+            print(f'Checked {len(accessible_urls) + len(inaccessible_urls)} URLs', end='\r')
+
+        time2 = time.time()
+
+    print(f'Took {time2 - time1:.2f} s')
+    print(f'Accessible URLs: {len(accessible_urls)}')
+    print(f'Inaccessible URLs: {len(inaccessible_urls)}')
+    print("Accessible URLs: {}".format(accessible_urls))
+    print("Inaccessible URLs: {}".format(inaccessible_urls))
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    bucket_name = get_user_input()
+    if bucket_name:
+        fetch_web_pages(bucket_name, TIMEOUT)
