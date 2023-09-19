d5a40b73f6f88f130d1b4c215b1248cc948f9979
Mustafa Bebek <mmustafab324@gmail.com>
2023-09-19 14:30:50 +0300

    generate regex to restrictions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import time
 from cloud_providers.aws_regions import REGIONS
 from restricts import aws_is_valid_bucket_name
+import itertools
 
 CONNECTIONS = 100
 TIMEOUT = 5
@@ -15,10 +16,15 @@ def get_user_input():
         else:
             print("Invalid input. Please enter a valid bucket name.")
 
+def generate_bucket_name_variations(bucket_name):
+    allowed_characters = "abcdefghijklmnopqrstuvwxyz0123456789.-"
+    character_variations = itertools.product(allowed_characters, repeat=len(bucket_name))
+    variations = [''.join(chars) for chars in character_variations]
+    return variations
 
 def fetch_web_pages(bucket_name, timeout):
-    urls = [f'https://{bucket_name}.s3.{region}.amazonaws.com' or f'https://{bucket_name}.s3-{region}.amazonaws.com' for region in REGIONS]
-
+    variations = generate_bucket_name_variations(bucket_name)
+    urls = [f'https://{variation}.s3.{region}.amazonaws.com' or f'https://{variation}.s3-{region}.amazonaws.com' for region in REGIONS for variation in variations]
 
     def check_url(url):
         try:
--- restricts.py
+++ restricts.py
@@ -1,40 +1,27 @@
 import re
 
 def aws_is_valid_bucket_name(bucket_name):
-
-    if not 3 <= len(bucket_name) <= 63:
-        print("Bucket name length must be between 3 and 63 characters.")
+    pattern = r"^(?!xn--|sthree-|sthree-configurator)[a-zA-Z0-9][a-z0-9.-]{1,61}[a-z0-9](?<!-s3alias|--ol-s3)$"
+    if re.match(pattern, bucket_name):
+        return True
+    else:
+        print("Bucket name is not valid.")
         return False
 
+def azure_is_valid_blob_name(blob_name):
+        pattern = r"^(?!.*[/\.]{2,})[a-z0-9\.\-/]{1,1024}$"
 
-    if not re.match(r'^[a-zA-Z0-9]', bucket_name):
-        print("Bucket name must start with a letter or number.")
+        if re.match(pattern, blob_name):
+            return True
+        else:
+            print("Invalid blob name.")
+            return False
+
+def gcp_is_valid_storage_name(bucket_name):
+    pattern = r"^(?!goog)(?!.*\.goog)(?!.*\.\d+\.\d+\.\d+\.\d+)(?!.*google)[a-z0-9][a-z0-9\._-]{1,61}[a-z0-9]$"
+
+    if re.match(pattern, bucket_name):
+        return True
+    else:
+        print("Invalid Google Cloud Storage name.")
         return False
-
-
-    if '..' in bucket_name:
-        print("Bucket name must not contain two adjacent periods.")
-        return False
-
-
-    if re.match(r'^\d+\.\d+\.\d+\.\d+$', bucket_name):
-        print("Bucket name cannot be in the format of an IP address.")
-        return False
-
-
-    if bucket_name.startswith(('xn--', 'sthree-', 'sthree-configurator')):
-        print("Bucket name cannot start with 'xn--', 'sthree-', or 'sthree-configurator'.")
-        return False
-
-
-    if bucket_name.endswith(('-s3alias', '--ol-s3')):
-        print("Bucket name cannot end with '-s3alias' or '--ol-s3'.")
-        return False
-
-
-    if not re.match(r'^[a-z0-9.-]+$', bucket_name):
-        print("Bucket name can only contain lowercase letters, numbers, periods, and hyphens.")
-        return False
-
-    return True
-
